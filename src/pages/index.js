d7bee3b6fc706dea4c7eb5bba7d6fc16c158fa80
Mprystal <mjprystal@gmail.com>
2021-10-05 12:55:36 -0400

    add price form/section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Nav from "../components/Nav/Nav.js";
 import Footer from "../components/Footer/Footer";
+import Pricing from "../components/Pricing/Pricing.js";
 
 export default function Home() {
   return (
     <>
       <Nav lang={true} />
+      <Pricing />
       <Footer />
     </>
   );
