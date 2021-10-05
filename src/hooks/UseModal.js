db9e126c33d1e101f1b844871813f725906423ad
Moddoo <>
2021-10-05 11:18:11 -0500

    refactor/form

--- src/hooks/UseModal.js
+++ src/hooks/UseModal.js
@@ -2,12 +2,8 @@ import { useCallback, useState } from "react";
 
 const UseModal = () => {
   const [showModal, setShowModal] = useState(false);
-
-  // const openModal = useCallback(() => setShowModal(true), []);
-  // const closeModal = useCallback(() => setShowModal(false), []);
   const toggleModal = useCallback(() => setShowModal(!showModal), [showModal]);
 
-  // return { showModal, openModal, closeModal };
   return { showModal, toggleModal };
 };
 
